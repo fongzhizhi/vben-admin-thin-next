86eb298df7124a94e697a0a80c357df249ee8f16
fongzhizhi <1837486996@qq.com>
2021-10-03 17:34:22 +0800

    docs: 配置文件添加注释

---
+++ src/test.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>{{ vitePort }}</div>
+</template>
+
+<script lang="ts">
+  import { defineComponent, ref } from 'vue';
+
+  export default defineComponent({
+    name: 'TestApp',
+    setup() {
+      const vitePort = ref(import.meta.env.VITE_PORT);
+      return {
+        vitePort,
+      };
+    },
+  });
+</script>
